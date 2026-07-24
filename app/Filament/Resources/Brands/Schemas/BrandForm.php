019f5bb31f8c653fd9653de4c756d71eaba0aa65
BassWindu-7 <tbdick504@gmail.com>
2026-07-24 02:33:46 -0600

    Migracion de Brand
    Generacion del modelo y resource de la tabla Brand

---
+++ app/Filament/Resources/Brands/Schemas/BrandForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class BrandForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('description')
+                    ->default(null),
+                Toggle::make('status')
+                    ->required(),
+            ]);
+    }
+}
